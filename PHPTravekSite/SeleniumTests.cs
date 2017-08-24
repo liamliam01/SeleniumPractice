041d4c70ffc4ec3e7b24ac2da77e692e54447d22
PST Ltd <PST Ltd@DESKTOP-FA1GIS4>
2017-08-24 19:28:03 +0100

    base code set

---
+++ PHPTravekSite/SeleniumTests.cs
@@ -0,0 +1,54 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+using NUnit.Framework;
+using System.Threading;
+
+namespace PHPTravekSite
+{
+
+    namespace SeleniumTests
+    {
+        // [Parallelizable]
+        [TestFixture(typeof(InternetExplorerDriver))]
+        [TestFixture(typeof(ChromeDriver))]
+        public class TestWithMultipleBrowsers<TWebDriver> where TWebDriver : IWebDriver, new()
+        {
+            public IWebDriver driver;
+
+            [SetUp]
+            public void CreateDriver()
+            {
+                this.driver = new TWebDriver();
+            }
+
+            [Test]
+            public void GoogleTest()
+            {
+                driver.Navigate().GoToUrl("http://www.google.com/");
+                IWebElement query = driver.FindElement(By.Name("q"));
+                query.SendKeys("Bread" + Keys.Enter);
+
+
+                driver.Quit();
+            }
+        }
+    }
+
+
+
+
+}
+
+
+
+
+
+
+
--- PHPTravekSite/TestBase.cs
+++ PHPTravekSite/TestBase.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace PHPTravekSite
 {
-    public class Class1
+    class TestBase
     {
     }
 }
