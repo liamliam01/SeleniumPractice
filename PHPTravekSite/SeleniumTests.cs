9204271072880f1afd87aac03eebd03d9a018685
PST Ltd <PST Ltd@DESKTOP-FA1GIS4>
2017-08-25 00:00:16 +0100

    Works in chrme but not ie

--- PHPTravekSite/SeleniumTests.cs
+++ PHPTravekSite/SeleniumTests.cs
@@ -46,22 +46,37 @@ namespace PHPTravekSite
 
             public void LaunchSite()
             {
+                               
                 driver.Navigate().GoToUrl("http://www.phptravels.net");
 
                
                 IWebElement myAccount = driver.FindElement(By.XPath("/html/body/div[2]/div/div/div[2]/ul/li[2]"));
+                Thread.Sleep(1000);
+                myAccount.Click();
+
+                IWebElement login = driver.FindElement(By.XPath("/html/body/div[2]/div/div/div[2]/ul/li[2]/ul/li[1]/a"));
+                Thread.Sleep(10000);
+                login.Click();
+
+                IWebElement email = driver.FindElement(By.XPath("//*[@id=\"loginfrm\"]/div[4]/div/div[1]/input"));
+                email.Click();
+                email.SendKeys("user@phptravels.com");
+
+                IWebElement password = driver.FindElement(By.XPath("//*[@id=\"loginfrm\"]/div[4]/div/div[2]/input"));
+                password.SendKeys("demouser");
+
+                IWebElement btnLogin = driver.FindElement(By.XPath("//*[@id=\"loginfrm\"]/div[4]/button"));
+                btnLogin.Click();
+
+                Thread.Sleep(2000);
+
+                string nameConfirm = driver.FindElement(By.XPath("/html/body/div[3]/div[1]/div/div[1]/h3")).Text;
+                Assert.IsTrue(nameConfirm.Contains("Hi, John Smith"), nameConfirm + "not found");
+                Console.Write("user welcome is found, login successful");
 
                 Thread.Sleep(1000);
 
-                myAccount.Click();
-                IWebElement login = driver.FindElement(By.XPath("/html/body/div[2]/div/div/div[2]/ul/li[2]/ul/li[1]/a"));
-
-                Thread.Sleep(10000);
-
-                login.Click();
-
-                
-
+                driver.Close();
 
             }
         }
